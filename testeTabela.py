34dc1c34388a1c03ddbb8334d5b31dd98ddada6c
Acso Notebook <glendaeea1@gmail.com>
2024-04-13 12:17:23 -0300

    setted a new background to fit the shoes store

--- testeTabela.py
+++ testeTabela.py
@@ -6,12 +6,17 @@ tabelaProdutos = tabelaPrincipal.getTabelaProdutos()
 
 produto = Produto("Camisa", "100", "Amarela")
 produto2 = Produto("Camisa", "100", "Vermelha")
+produto3 = Produto("Camisa", "200", "Preto")
 
 produto = tabelaProdutos.insert(produto)
 produto2 = tabelaProdutos.insert(produto2)
+produto3 = tabelaProdutos.insert(produto3)
 
-print(tabelaProdutos.tabela)
+for produtoa in tabelaProdutos.tabela.items():
+    print(produtoa)
 
 tabelaProdutos.delete(produto.idProduto)
 
-print(tabelaProdutos.tabela)
+print("DEPOIS DE DELETAR")
+for produtoa in tabelaProdutos.tabela.items():
+    print(produtoa)
